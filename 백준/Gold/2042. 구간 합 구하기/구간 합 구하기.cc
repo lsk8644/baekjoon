5d8080d02af44ccdddb39e464f93392e9cec4991
lsk8644 <105280082+lsk8644@users.noreply.github.com>
2024-10-19 00:14:52 +0900

    [Gold I] Title: 구간 합 구하기, Time: 192 ms, Memory: 33272 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -12,35 +12,35 @@ long long Sum(int L, int R, int nodeNum, int start, int end);
 
 int main()
 {
-	cin.tie(0);
-	ios_base::sync_with_stdio(0);
-	cin>>N>>M>>K;
-    
-    for (int i=1;i<=N;i++)
-        cin>>brr[i];
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin >> N >> M >> K;
+
+    for (int i = 1; i <= N; i++)
+        cin >> brr[i];
 
     init(1,1,N);
 
-    for (int i=0;i<M+K;i++)
+    for (int i = 0; i < M + K; i++)
     {
-        cin>>a>>b>>c;
-        long long diff=0;
+        cin >> a >> b >> c;
+        long long diff = 0;
         if (a==1)
         {
             diff = c - brr[b];
-            brr[b]=c;
+            brr[b] = c;
             update(1,1,N,b,diff);
         }
         else if (a==2)
         {
-            cout<<Sum(b,c,1,1,N)<<"\n";   
+            cout << Sum(b,c,1,1,N) << "\n";
         }
     }
 }
 
 long long init(int node,int start,int end)
 {
-    if (start==end)        
+    if (start==end)
         return arr[node]=brr[start];
     int mid=(start+end)/2;
     return arr[node]=init(node*2,start,mid)+init(node*2+1,mid+1,end);
@@ -48,10 +48,10 @@ long long init(int node,int start,int end)
 
 void update(int node,int start,int end,int index,long long diff)
 {
-    if (end < index || index < start) 
-        return; 
+    if (end < index || index < start)
+        return;
     arr[node] += diff;
-    if (start != end) 
+    if (start != end)
     {
         int mid = (start + end) / 2;
         update(2 * node, start, mid, index, diff);
@@ -61,10 +61,10 @@ void update(int node,int start,int end,int index,long long diff)
 
 long long Sum(int L, int R, int nodeNum, int start, int end)
 {
-        if(R < start || end < L) 
-            return 0;
-        if(L <= start && end <= R) 
-            return arr[nodeNum];
-        int mid = (start + end) / 2;
-        return Sum(L, R, nodeNum*2, start, mid) + Sum(L, R, nodeNum*2+1, mid+1, end);
+    if(R < start || end < L)
+        return 0;
+    if(L <= start && end <= R)
+        return arr[nodeNum];
+    int mid = (start + end) / 2;
+    return Sum(L, R, nodeNum*2, start, mid) + Sum(L, R, nodeNum*2+1, mid+1, end);
 }
