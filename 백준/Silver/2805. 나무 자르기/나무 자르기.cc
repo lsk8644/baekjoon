c3752e32186426f09a98126953496b698d3169f0
lsk8644 <105280082+lsk8644@users.noreply.github.com>
2024-10-20 14:23:02 +0900

    [Silver II] Title: 나무 자르기, Time: 160 ms, Memory: 9708 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -11,7 +11,7 @@ int main() {
     ll H[N];
     for (int i = 0; i < N; i++) cin >> H[i];
     sort(H, H + N);
-    ll low = 0, high = H[N - 1], mid = 0, chk = 1e9;
+    ll low = 0, high = H[N - 1], mid = 0;
     while (low <= high) {
         mid = (low + high +1) / 2; ll sum = 0;
         for (int i = 0; i < N; i++) if (H[i] > mid) sum += H[i] - mid;
