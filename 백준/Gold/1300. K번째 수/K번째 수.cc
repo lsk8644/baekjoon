eaedbf7697e55284662077dc83cc9f2a275ef8a6
lsk8644 <105280082+lsk8644@users.noreply.github.com>
2024-10-20 15:36:01 +0900

    [Gold I] Title: K번째 수, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.cc
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 using ll = long long;
 using namespace std;
-
 int main() {
-    cin.tie(0);
-    ios_base::sync_with_stdio(0);
+    cin.tie(0); ios_base::sync_with_stdio(0);
     ll N, K, ans = 0; cin >> N >> K;
     ll start = 1, end = K;
     while (start <= end) {
-        ll mid = start + (end - start) / 2, cnt = 0;
+        ll mid = ( start + end ) / 2, cnt = 0;
         for (int i = 1; i <= N; i++) cnt += min(mid / i, N);
         if (cnt < K) start = mid + 1;
         else { end = mid - 1; ans = mid; }
